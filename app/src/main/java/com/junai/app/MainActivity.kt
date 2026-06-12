68c286ebd4a759c2d0581a0e5f2a75a7fe87d3f8
pranavyadav9288-jpg <pranavyadav9288@gmail.com>
2026-06-12 23:58:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/junai/app/MainActivity.kt
+++ app/src/main/java/com/junai/app/MainActivity.kt
@@ -2,10 +2,33 @@ package com.junai.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        findViewById<android.widget.ImageButton>(R.id.menuButton).setOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
