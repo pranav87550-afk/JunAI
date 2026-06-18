10c76259b9a70658887b0d55b8051d205e875212
pranavyadav9288-jpg <pranavyadav9288@gmail.com>
2026-06-18 18:54:56 +0530

    Create BotExpression.kt

---
+++ app/src/main/java/com/junai/app/BotExpression.kt
@@ -0,0 +1,10 @@
+package com.junai.app
+
+enum class BotExpression {
+    NEURAL,      // Default - calm, focused eyes
+    HAPPY,       // Eyes curved up, big smile
+    THINKING,    // One eye squinted, small mouth
+    LISTENING,   // Wide eyes, neutral mouth
+    SPEAKING,    // Normal eyes, mouth animating
+    SLEEPING     // Eyes closed, flat mouth
+}
