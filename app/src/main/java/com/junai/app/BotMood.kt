6c8c481ae96be535e62f48e942dcdbe952c5b230
pranavyadav9288-jpg <pranavyadav9288@gmail.com>
2026-06-22 11:38:54 +0530

    Update BotMood.kt

--- app/src/main/java/com/junai/app/BotMood.kt
+++ app/src/main/java/com/junai/app/BotMood.kt
@@ -9,9 +9,29 @@ enum class BotMood {
 
 object BotMoodMessages {
     val messages = mapOf(
-        BotMood.SMILE  to listOf("Hi! 👋", "Hello! 😊", "Greetings! ✨"),
-        BotMood.SLEEPY to listOf("Don't disturb me😴", "ZZzz...", "I need rest 🥱"),
-        BotMood.DIZZY  to listOf("Slowly😵‍💫", "i..i..i🤮", "My head!😵"),
-        BotMood.ANGRY  to listOf("So you are ignoring me?🤨", "Hey I am here too🥺", "I am invisible 😠")
+        BotMood.SMILE to listOf(
+            "Hi! 👋",
+            "Hello! 😊",
+            "Hey there! ✨",
+            "What's up? 😄"
+        ),
+        BotMood.SLEEPY to listOf(
+            "Don't disturb me... 😴",
+            "ZZzz...",
+            "I need rest 🥱",
+            "So sleepy... 💤"
+        ),
+        BotMood.DIZZY to listOf(
+            "Slowly please! 😵‍💫",
+            "i...i...i 🤮",
+            "My head! 😵",
+            "Too fast! 🌀"
+        ),
+        BotMood.ANGRY to listOf(
+            "So you are ignoring me? 🤨",
+            "Hey, I am here too! 🥺",
+            "I am invisible? 😠",
+            "Notice me! 😤"
+        )
     )
 }
